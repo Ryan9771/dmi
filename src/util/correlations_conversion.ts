c8173784279d5e34f0d0689fb8bd3496a0894439
ryan <ryan.patel21@imperial.ac.uk>
2023-06-29 16:40:16 +0100

    Added data parser functions

---
+++ src/util/correlations_conversion.ts
@@ -0,0 +1,43 @@
+interface InnerDict {
+    [key: string]: number;
+}
+
+interface NestedDict {
+    [key: string]: InnerDict;
+}
+
+
+export function correlationsToArray(csvData: string): NestedDict {
+   /* Gets the rows and headers of the csv file */
+   let rows = csvData.split('\n');
+   let headers = rows[0].split(',');
+
+   let dict: NestedDict = {};
+
+   /* Creates an empty entry for each country name */
+   for (let i = 1; i < headers.length; i++) {
+       let header = headers[i].trim();
+       if (header === "") {
+           continue;
+       }
+       dict[header] = {};
+   }
+
+   /* Each country is assigned an inner dictionary with all other country
+       correlations */
+   for (let i = 1; i < rows.length; i++) {
+       let values = rows[i].split(',');
+       let country = values[0].trim();
+
+       let innerDict: InnerDict = {}
+
+       for (let j = 1; j < headers.length; j++) {
+           let header = headers[j].trim();
+           innerDict[header] = parseFloat(values[j]);
+       }
+
+       dict[country] = innerDict;
+   }
+
+   return dict 
+}
---
+++ src/util/csv_conversion.ts
@@ -0,0 +1,11 @@
+export function csvToArray(csv: string): string[][] {
+    const rows: string[] = csv.split('\n');
+    const dataArray: string[][] = [];
+
+    for (let i = 0; i < rows.length; i++) {
+        let row = rows[i].split(',');
+        dataArray.push(row);
+    }
+
+    return dataArray;
+}
