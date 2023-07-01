f42659252311c1afa0fbee704852ee16254d402e
ryan <ryan.patel21@imperial.ac.uk>
2023-07-01 13:07:48 +0100

    removed react strict mode causing useEffect to run twice

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import FilterButtons from "./components/buttons/FilterButtons";
 import DesktopTable from "./components/table/DesktopTable";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { csvToArray } from "./util/csv_conversion";
 
 export default function App() {
+  const [dmiCsv, setDmiCsv] = useState<string[][]>([]);
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch("/src/assets/data/dmi_2023_clusters.csv");
         const csvString = await response.text();
-        console.log(csvString);
+        csvToArray(csvString);
       } catch (error) {
         console.log(error);
       }
