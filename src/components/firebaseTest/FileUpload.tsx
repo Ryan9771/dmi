819fad4fd83520dfed2f43aa56ddb82898b04351
ryan <ryan.patel21@imperial.ac.uk>
2023-07-30 00:57:03 +0100

    implemented file name retrieval. ToFix: prevent addition to list upon save by maybe a set

--- src/components/firebaseTest/FileUpload.tsx
+++ src/components/firebaseTest/FileUpload.tsx
@@ -1,28 +1,53 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { storage } from "../../services/firebase.config";
-import { ref, uploadBytes } from "firebase/storage";
+import { ref, uploadBytes, getDownloadURL, listAll } from "firebase/storage";
 import { v4 } from "uuid";
 
 function FileUpload() {
   const [file, setFile] = useState<File | null>(null);
+  const [fileRefs, setFileRefs] = useState<string[]>([]);
+
+  const filesListRef = ref(storage, "files/");
+
   const uploadFile = () => {
     if (!file) return;
 
     const fileRef = ref(storage, `files/${file.name + v4()}`);
-    uploadBytes(fileRef, file).then(() => {
-      alert("File uploaded successfully!");
+    uploadBytes(fileRef, file).then((snapshot) => {
+      // Entering here means file uploaded successfully
+      getDownloadURL(snapshot.ref).then((url) => {
+        setFileRefs((prev) => [...prev, url]);
+      });
     });
   };
 
+  useEffect(() => {
+    listAll(filesListRef).then((response) => {
+      response.items.forEach((itemRef) => {
+        getDownloadURL(itemRef).then((url) => {
+          setFileRefs((prev) => [...prev, url]);
+        });
+      });
+    });
+  }, []);
+
   return (
-    <div className="flex items-center justify-center gap-4">
-      <input
-        type="file"
-        onChange={(event: any) => setFile(event.target.files[0])}
-      />
-      <button className="p-3 rounded-lg" onClick={uploadFile}>
-        Upload
-      </button>
+    <div className="flex flex-col items-center gap-5">
+      <div className="flex items-center justify-center gap-4">
+        <input
+          type="file"
+          onChange={(event: any) => setFile(event.target.files[0])}
+        />
+        <button className="p-3 rounded-lg" onClick={uploadFile}>
+          Upload
+        </button>
+      </div>
+
+      <div className="flex flex-col gap-3 items-center">
+        {fileRefs.map((fileReference) => {
+          return <div>Url: {fileReference}</div>;
+        })}
+      </div>
     </div>
   );
 }
