65b6ce5dabc82b8de6a6ed075554fbbee7fc0477
ryan <ryan.patel21@imperial.ac.uk>
2023-08-19 15:13:24 +0100

    Added logic to move active files to achive. Moved html to new upload component

--- src/components/upload/Upload.tsx
+++ src/components/upload/Upload.tsx
@@ -7,16 +7,15 @@ import {
   getDownloadURL,
   listAll,
   StorageReference,
+  deleteObject,
 } from "firebase/storage";
+import { v4 } from "uuid";
+import { FileEarmarkText } from "react-bootstrap-icons";
 
 function Upload() {
   const [file, setFile] = useState<File | null>(null);
   const [activeFileRef, setActiveFileRef] = useState<string>("");
-
-  /* References */
   const activeFileLocation = ref(storage, "active/");
-  const archiveFileLocation = ref(storage, "archive/");
-  const otherFileLocation = ref(storage, "others/");
 
   useEffect(() => {
     listAll(activeFileLocation).then((response) => {
@@ -25,26 +24,108 @@ function Upload() {
       );
     });
   }, [file]);
-  /* 
-    
-    1. Get everything in the active directory on firebase
-    2. On upload, check if file matches type and name
-        2a. If it doesnt, then upload to others
-        2b. If it does, then check if the file exists in the active directory
-            2b1. If it does, then move that file to archive
-            2b2. Upload the new file to active
-        
-    
-    */
-  const uploadFile = () => {
-    if (!file) return;
 
-    if (file.type !== "text/csv") {
-      return;
+  const uploadFile = () => {
+    if (!file || file.type !== "text/csv") return;
+    const fileRef = ref(storage, `active/${file.name}`);
+
+    /* If there is an active file, move it to archive */
+    if (activeFileRef) {
+      const srcFileRef = ref(storage, `active/${activeFileRef}`);
+      const destFileRef = ref(storage, `archive/${activeFileRef + v4()}`);
+      getDownloadURL(srcFileRef)
+        .then((url) => fetch(url))
+        .then((response) => response.blob())
+        .then((blob) => uploadBytes(destFileRef, blob))
+        .then(() => deleteObject(srcFileRef))
+        .then(() => console.log("File moved and deleted"))
+        .catch((error) => console.log("Error moving file: ", error));
     }
+    // Upload file to active directory
+    uploadBytes(fileRef, file).then(() => {
+      setActiveFileRef(file.name);
+    });
   };
 
-  return <p>This is the upload component</p>;
+  return (
+    <div className={getStyle(styles, "ctn")}>
+      <div className={getStyle(styles, "uploadCtn")}>
+        <input
+          type="file"
+          id="fileUpload"
+          accept=".csv"
+          onChange={(event: any) => setFile(event.target.files[0])}
+          className="hidden"
+        />
+
+        <div className={getStyle(styles, "uploadSubCtn")}>
+          <label htmlFor="fileUpload">
+            <FileEarmarkText className={getStyle(styles, "icon")} />
+          </label>
+
+          {!file && (
+            <p className={getStyle(styles, "uploadText")}>
+              Upload a .csv or .xlsx file
+            </p>
+          )}
+
+          {file && (
+            <p className={getStyle(styles, "uploadText")}>{file.name}</p>
+          )}
+        </div>
+
+        {file && (
+          <button
+            onClick={uploadFile}
+            className={getStyle(styles, "uploadBtn")}
+          >
+            Upload
+          </button>
+        )}
+      </div>
+    </div>
+  );
 }
+
+const styles = {
+  ctn: ["w-full", "h-full", "flex", "justify-center", "items-center"],
+  uploadCtn: [
+    "flex",
+    "flex-col",
+    "items-center",
+    "justify-center",
+    "rounded-lg",
+    "gap-6",
+    "bg-button-gray",
+    "border-[4px]",
+    "border-imperial-blue",
+    "w-80",
+    "sm:w-96",
+    "h-60",
+    "shadow-md",
+    "trans",
+  ],
+  uploadBtn: [
+    "bg-imperial-blue",
+    "text-white",
+    "text-sm",
+    "rounded-md",
+    "px-3",
+    "py-1",
+    "font-normal",
+    "shadow-sm",
+    "hover:scale-110",
+  ],
+  uploadSubCtn: ["flex", "flex-col", "items-center", "gap-4"],
+  icon: [
+    "fill-imperial-blue",
+    "w-10",
+    "h-10",
+    "cursor-pointer",
+    "hover:scale-110",
+    "trans",
+  ],
+  uploadText: ["font-semibold", "text-md"],
+};
 
 export default Upload;
