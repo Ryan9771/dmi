741f09c932c425a99ae3807955af38b8a2b43257
ryan <ryan.patel21@imperial.ac.uk>
2023-09-12 00:13:21 +0100

    Added full navbar for mobile

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -9,15 +9,21 @@ function Navbar() {
           Imperial College Business School
         </p>
       </div>
+      <div className={getStyle(style, "subNavbar")}>
+        <p className={getStyle(style, "subNavbarItem")}>About</p>
+        <p className={getStyle(style, "subNavbarItem")}>Visualisation</p>
+      </div>
     </div>
   );
 }
 
 const style = {
-  ctn: ["flex", "items-center", "justify-between", "px-2", "py-1"],
-  logoIcon: ["font-semibold", "text-6xl"],
-  logoCtn: ["flex", "items-center", "gap-4"],
-  logoText: ["w-32", "font-semibold"],
+  ctn: ["flex", "w-full", "items-center", "justify-between", "px-2", "py-1"],
+  logoIcon: ["font-semibold", "text-5xl"],
+  logoCtn: ["flex", "items-center", "gap-3", "cursor-pointer"],
+  logoText: ["w-32", "font-semibold", "text-sm"],
+  subNavbar: ["flex", "items-center", "gap-4"],
+  subNavbarItem: ["font-semibold", "text-xs", "cursor-pointer"],
 };
 
 export default Navbar;
