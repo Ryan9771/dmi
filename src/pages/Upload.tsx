fbd88d3e9f47d5fc122a27f8d1805d441226b665
ryan <ryan.patel21@imperial.ac.uk>
2023-10-11 18:11:38 +0100

    Accessibility wip

--- src/pages/Upload.tsx
+++ src/pages/Upload.tsx
@@ -1,128 +1,128 @@
-import getStyle from "../Styles";
-import { useEffect, useState } from "react";
-import { storage } from "../services/firebase.config";
-import {
-  ref,
-  uploadBytes,
-  listAll,
-  StorageReference,
-  deleteObject,
-} from "firebase/storage";
-import { v4 } from "uuid";
-import { FileEarmarkText } from "react-bootstrap-icons";
+// import getStyle from "../Styles";
+// import { useEffect, useState } from "react";
+// // import { storage } from "../services/firebase.config";
+// import {
+//   ref,
+//   uploadBytes,
+//   listAll,
+//   StorageReference,
+//   deleteObject,
+// } from "firebase/storage";
+// import { v4 } from "uuid";
+// import { FileEarmarkText } from "react-bootstrap-icons";
 
-function Upload() {
-  const [file, setFile] = useState<File | null>(null);
-  const [activeFileName, setActiveFileName] = useState<string>("");
-  const activeFileLocation = ref(storage, "active/");
+// function Upload() {
+//   const [file, setFile] = useState<File | null>(null);
+//   const [activeFileName, setActiveFileName] = useState<string>("");
+//   // const activeFileLocation = ref(storage, "active/");
 
-  useEffect(() => {
-    listAll(activeFileLocation).then((response) => {
-      response.items.forEach((item: StorageReference) =>
-        setActiveFileName(item.name)
-      );
-    });
-  }, [file]);
+//   useEffect(() => {
+//     listAll(activeFileLocation).then((response) => {
+//       response.items.forEach((item: StorageReference) =>
+//         setActiveFileName(item.name)
+//       );
+//     });
+//   }, [file]);
 
-  const uploadFile = () => {
-    if (!file || file.type !== "text/csv") return;
-    const activeFileRef = ref(storage, `active/${file.name}`);
-    const archiveFileRef = ref(storage, `archive/${file.name + v4()}`);
+//   const uploadFile = () => {
+//     if (!file || file.type !== "text/csv") return;
+//     const activeFileRef = ref(storage, `active/${file.name}`);
+//     const archiveFileRef = ref(storage, `archive/${file.name + v4()}`);
 
-    // TODO: Check if it is a particular file name
+//     // TODO: Check if it is a particular file name
 
-    /* If there is an active file, delete it */
-    if (activeFileName) {
-      deleteObject(ref(storage, `active/${activeFileName}`));
-    }
-    // Upload file to active directory
-    uploadBytes(activeFileRef, file).then(() => {
-      setActiveFileName(file.name);
-    });
+//     /* If there is an active file, delete it */
+//     if (activeFileName) {
+//       deleteObject(ref(storage, `active/${activeFileName}`));
+//     }
+//     // Upload file to active directory
+//     uploadBytes(activeFileRef, file).then(() => {
+//       setActiveFileName(file.name);
+//     });
 
-    // Upload file to archive directory as backup
-    uploadBytes(archiveFileRef, file).then(() => {});
-  };
+//     // Upload file to archive directory as backup
+//     uploadBytes(archiveFileRef, file).then(() => {});
+//   };
 
-  return (
-    <div className={getStyle(styles, "ctn")}>
-      <div className={getStyle(styles, "uploadCtn")}>
-        <input
-          type="file"
-          id="fileUpload"
-          accept=".csv"
-          onChange={(event: any) => setFile(event.target.files[0])}
-          className="hidden"
-        />
+//   return (
+//     <div className={getStyle(styles, "ctn")}>
+//       <div className={getStyle(styles, "uploadCtn")}>
+//         <input
+//           type="file"
+//           id="fileUpload"
+//           accept=".csv"
+//           onChange={(event: any) => setFile(event.target.files[0])}
+//           className="hidden"
+//         />
 
-        <div className={getStyle(styles, "uploadSubCtn")}>
-          <label htmlFor="fileUpload">
-            <FileEarmarkText className={getStyle(styles, "icon")} />
-          </label>
+//         <div className={getStyle(styles, "uploadSubCtn")}>
+//           <label htmlFor="fileUpload">
+//             <FileEarmarkText className={getStyle(styles, "icon")} />
+//           </label>
 
-          {!file && (
-            <p className={getStyle(styles, "uploadText")}>
-              Upload a .csv or .xlsx file
-            </p>
-          )}
+//           {!file && (
+//             <p className={getStyle(styles, "uploadText")}>
+//               Upload a .csv or .xlsx file
+//             </p>
+//           )}
 
-          {file && (
-            <p className={getStyle(styles, "uploadText")}>{file.name}</p>
-          )}
-        </div>
+//           {file && (
+//             <p className={getStyle(styles, "uploadText")}>{file.name}</p>
+//           )}
+//         </div>
 
-        {file && (
-          <button
-            onClick={uploadFile}
-            className={getStyle(styles, "uploadBtn")}
-          >
-            Upload
-          </button>
-        )}
-      </div>
-    </div>
-  );
-}
+//         {file && (
+//           <button
+//             onClick={uploadFile}
+//             className={getStyle(styles, "uploadBtn")}
+//           >
+//             Upload
+//           </button>
+//         )}
+//       </div>
+//     </div>
+//   );
+// }
 
-const styles = {
-  ctn: ["w-full", "h-full", "flex", "justify-center", "items-center"],
-  uploadCtn: [
-    "flex",
-    "flex-col",
-    "items-center",
-    "justify-center",
-    "rounded-lg",
-    "gap-6",
-    "bg-button-gray",
-    "border-[4px]",
-    "border-imperial-blue",
-    "w-80",
-    "sm:w-96",
-    "h-60",
-    "shadow-md",
-    "trans",
-  ],
-  uploadBtn: [
-    "bg-imperial-blue",
-    "text-white",
-    "text-sm",
-    "rounded-md",
-    "px-3",
-    "py-1",
-    "font-normal",
-    "shadow-sm",
-    "hover:scale-110",
-  ],
-  uploadSubCtn: ["flex", "flex-col", "items-center", "gap-4"],
-  icon: [
-    "fill-imperial-blue",
-    "w-10",
-    "h-10",
-    "cursor-pointer",
-    "hover:scale-110",
-    "trans",
-  ],
-  uploadText: ["font-semibold", "text-md"],
-};
+// const styles = {
+//   ctn: ["w-full", "h-full", "flex", "justify-center", "items-center"],
+//   uploadCtn: [
+//     "flex",
+//     "flex-col",
+//     "items-center",
+//     "justify-center",
+//     "rounded-lg",
+//     "gap-6",
+//     "bg-button-gray",
+//     "border-[4px]",
+//     "border-imperial-blue",
+//     "w-80",
+//     "sm:w-96",
+//     "h-60",
+//     "shadow-md",
+//     "trans",
+//   ],
+//   uploadBtn: [
+//     "bg-imperial-blue",
+//     "text-white",
+//     "text-sm",
+//     "rounded-md",
+//     "px-3",
+//     "py-1",
+//     "font-normal",
+//     "shadow-sm",
+//     "hover:scale-110",
+//   ],
+//   uploadSubCtn: ["flex", "flex-col", "items-center", "gap-4"],
+//   icon: [
+//     "fill-imperial-blue",
+//     "w-10",
+//     "h-10",
+//     "cursor-pointer",
+//     "hover:scale-110",
+//     "trans",
+//   ],
+//   uploadText: ["font-semibold", "text-md"],
+// };
 
-export default Upload;
+// export default Upload;
